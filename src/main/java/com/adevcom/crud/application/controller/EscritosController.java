b60007adede7528a9506625ae6ef64f2f0065429
Lucas Agustín Guido <lukasredfield@gmail.com>
2023-10-21 18:15:28 -0300

    fix controllers GET,GETALL,POST and DELETE done

--- src/main/java/com/adevcom/crud/application/controller/EscritosController.java
+++ src/main/java/com/adevcom/crud/application/controller/EscritosController.java
@@ -7,7 +7,6 @@ import com.adevcom.crud.application.model.EscritosResponse;
 import com.adevcom.crud.domain.model.*;
 import com.adevcom.crud.domain.port.EscritosServicePort;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,7 +34,7 @@ public class EscritosController {
 
         EscritosResponse escritosResponse = EscritosRestMapper.INSTANCE.toEscritosResponse(escritos);
         this.escritosServicePort.createEscritos(escritos);
-        return ResponseEntity.ok().body(escritosResponse);
+        return ResponseEntity.ok().body(escritosResponse);  // ARREGLAR LA RESPUESTA DE "escritosResponse" !!!!!!!!!!!
     }
 
 
@@ -53,10 +52,11 @@ public class EscritosController {
     }
 
     @DeleteMapping("delete/{id}")
-    public ResponseEntity<EscritosResponse> deleteEscritos(@RequestParam Long id){
+    public ResponseEntity<EscritosResponse> deleteEscritos(@PathVariable Long id) {
         this.escritosServicePort.deleteEscritos(id);
         return ResponseEntity.ok().body(null);
     }
+
 
 //    @PutMapping("/update/{id}")
 //    public ResponseEntity<EscritosResponse> updateTask(@RequestBody EscritosRequest taskRequest, @PathVariable Long id){
